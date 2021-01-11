4370e4c743e5e79ffa5c5236afa320d0cf8888c8
arthur-FD <arthur@fifthdelta.com>
2021-01-11 19:03:51 +0000

    add function to get YTD data for OEM view

---
+++ year_to_date_oem_view/__init__.py
@@ -0,0 +1,35 @@
+import logging
+
+import azure.functions as func
+import logging
+import os
+
+import azure.functions as func
+import pandas as pd
+import snowflake.connector
+import yaml
+import pathlib
+from year_to_date_oem_view.utils.config_loader import ConfigLoader
+
+
+def main(req: func.HttpRequest) -> func.HttpResponse:
+    logging.info('Python HTTP trigger function processed a request.')
+    year = req.params.get('year')
+    with open(pathlib.Path(__file__).parent / "conf/parameter.yml", "r") as file:
+        parameters = yaml.load(file, Loader=ConfigLoader)
+    with open(pathlib.Path(__file__).parent / "conf/funct_query.sql", "r") as file:
+        core_query = file.read()
+
+    conn = snowflake.connector.connect(
+        user=os.environ["USER_SF"],
+        password=os.environ["PSW_SF"],
+        account=os.environ["ACCOUNT_SF"],
+        **parameters["snowflake_config"]
+    ) 
+
+    cur = conn.cursor()
+    cur.execute(core_query,{'LAST_YEAR_AVAILABLE':int(year)})
+    core_data = cur.fetch_pandas_all()
+    
+    return func.HttpResponse(body=core_data.to_json(date_format="iso", orient="split"))
+
